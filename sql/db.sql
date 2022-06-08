c9f1e9c260ce3f6d0f6d287e200a244807331115
9590 <9590@yeah.net>
2022-06-08 18:52:02 +0800

    Update db.sql

--- sql/db.sql
+++ sql/db.sql
@@ -19,6 +19,18 @@ create table ROLES (
    constraint PK_ROLES primary key (ID)
 );
 
+create table SONGS (
+   ID                   SERIAL               not null,
+   ALBUM_ID             INT4                 null,
+   NAME                 VARCHAR(100)         null,
+   DURATION             TIME                 null,
+   SONG_LINK            TEXT                 null,
+   THUMBNAIL            TEXT                 null,
+   POPULARITY           INT4                 null,
+   GENRE                TEXT                 null,
+   constraint PK_SONGS primary key (ID)
+);
+
 -- INSERT USERS DATA
 INSERT INTO users(rol_id, username, name, mail, password, thumbnail)
     VALUES(1, 'carlosvldz', 'carlos', 'me@carlosvldz.com', '1234', 'link');
